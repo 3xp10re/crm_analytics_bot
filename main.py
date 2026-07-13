c9d706928712254df48b71ff4cc59b4a32e03c60
3xp10re <3xp10re@protonmail.com>
2026-07-13 11:15:04 +0300

    list of commands added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import aiogram
 import matplotlib
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.types import BotCommand
 from dotenv import load_dotenv
 
 from app.handlers import router
@@ -14,7 +15,39 @@ async def main() -> None:
     bot = Bot(token=os.getenv("BOT_TOKEN"))
     dp = Dispatcher()
     dp.include_router(router)
+    await set_bot_commands(bot)
     await dp.start_polling(bot)
+
+
+async def set_bot_commands(bot: Bot) -> None:
+    commands = [
+        BotCommand(
+            command="start",
+            description="Запуск бота",
+        ),
+        BotCommand(
+            command="metrics",
+            description="Метрики за текущий месяц",
+        ),
+        BotCommand(
+            command="top_managers",
+            description="Топ менеджеров",
+        ),
+        BotCommand(
+            command="status_report",
+            description="Отчёт по статусам",
+        ),
+        BotCommand(
+            command="chart_revenue",
+            description="График выручки",
+        ),
+        BotCommand(
+            command="recommendations",
+            description="Рекомендации по CRM",
+        ),
+    ]
+
+    await bot.set_my_commands(commands)
 
 
 if __name__ == "__main__":
