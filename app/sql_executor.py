5616c5410488622504ef350fe5168c648d91aa0d
3xp10re <3xp10re@protonmail.com>
2026-08-02 09:11:00 +0300

    added time and lenght limit for sql executor

--- app/sql_executor.py
+++ app/sql_executor.py
@@ -1,10 +1,54 @@
+from typing import Any
+from psycopg import errors
+
 from app.database import get_connection
 
 
-def execute_sql(sql: str) -> list[dict]:
-    with get_connection() as connection:
-        with connection.cursor() as cursor:
-            cursor.execute(sql)
-            rows = cursor.fetchall()
+MAX_ROWS = 100
+STATEMENT_TIMEOUT_MS = 5000
 
-    return rows
+
+class SQLExecutionError(Exception):
+    pass
+
+
+def execute_sql(sql: str) -> list[dict]:
+    try:
+        with get_connection() as connection:
+            with connection.transaction():
+                with connection.cursor() as cursor:
+                    cursor.execute("SET TRANSACTION READ ONLY")
+                    cursor.execute(
+                        f"""
+                        SET LOCAL statement_timeout =
+                        {STATEMENT_TIMEOUT_MS}
+                        """
+                    )
+
+                    cursor.execute(sql)
+
+                    if cursor.description is None:
+                        raise SQLExecutionError(
+                            "Запрос не вернул данные."
+                        )
+                    
+                    rows = cursor.fetchmany(MAX_ROWS+1)
+
+                    if len(rows) > MAX_ROWS:
+                        rows = rows[:MAX_ROWS]
+
+                    return [
+                        dict(row)
+                        for row in rows
+                    ]
+
+    except errors.QueryCanceled as error:
+        raise SQLExecutionError(
+            "Превышено время выполнения запроса."
+        ) from error
+
+    except SQLExecutionError:
+        raise
+
+    except SQLExecutionError:
+        raise
